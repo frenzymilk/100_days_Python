8ac985120f3a33c834711f60000df9eda8de24f5
pernelle.mensah@gmail.com <pernelle.mensah@gmail.com>
2023-08-08 16:03:41 +0200

    [end] Day 1

---
+++ day1_bandnameGenerator.py
@@ -0,0 +1,12 @@
+city_name = None
+pet_name = None
+
+print("Welcome to the Band Name Generator.")
+
+print("What's name of the city you grew up in?")
+city_name = input()
+
+print("What's your pet's name?")
+pet_name = input()
+
+print("Your band name could be " + city_name + " " + pet_name)
